9e7fafd044956e8d7b62fa6576b636fbbabf0ff1
shroukgamall17 <shrouqgamal17@gmail.com>
2025-01-18 22:48:37 +0200

    appointments

--- src/Store/axiosInstance/APIconfigrations.tsx
+++ src/Store/axiosInstance/APIconfigrations.tsx
@@ -1,8 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-  //baseURL: "http://localhost:3001/",
-  baseURL: "https://daily-employee-tasks-backend.vercel.app/",
+  baseURL: "http://localhost:3001/",
+//  baseURL: "https://daily-employee-tasks-backend.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
